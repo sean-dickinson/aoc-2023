bdcb8064408d351df4698a04c74d51636e46a8a0
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2023-12-04 20:00:16 -0500

    feat: passing

--- 04.rb
+++ 04.rb
@@ -1,11 +1,98 @@
 module Day04
+  class ScratchOffCard
+    class << self
+      def from_string(string)
+        card, numbers = string.split(":")
+        winning_numbers, played_numbers = numbers.split("|").map do |number_string|
+          number_string.split.map(&:to_i)
+        end
+        card_number = card.split.last.to_i
+        new(card_number:, winning_numbers:, played_numbers:)
+      end
+    end
+
+    attr_reader :card_number, :winning_numbers, :played_numbers
+
+    def initialize(winning_numbers:, played_numbers:, card_number: 1)
+      @card_number = card_number
+      @winning_numbers = winning_numbers
+      @played_numbers = played_numbers
+    end
+
+    def ==(other)
+      return false unless other.is_a?(ScratchOffCard)
+      card_number == other.card_number &&
+        winning_numbers == other.winning_numbers &&
+        played_numbers == other.played_numbers
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def score
+      return 0 if matches == 0
+      2**(matches - 1)
+    end
+
+    def matches
+      (@winning_numbers & @played_numbers).size
+    end
+  end
+
+  class ScratchOffGame
+    def initialize(cards:)
+      @cards = create_hash_from_cards(cards)
+    end
+
+    def cards
+      @cards.values
+    end
+
+    def all_cards
+      @cards.values + cards_won
+    end
+
+    private
+
+    def cards_won
+      @cards.values.map do |card|
+        get_cards_won_from_card(card)
+      end.flatten
+    end
+
+    def create_hash_from_cards(cards)
+      hash = {}
+      cards.each do |card|
+        hash[card.card_number] = card
+      end
+      hash
+    end
+
+    def get_card_by_number(number)
+      @cards[number]
+    end
+
+    def get_cards_won_from_card(card)
+      return [] if card.matches == 0
+      card.matches.times.map do |n|
+        next_card_number = card.card_number + n + 1
+        card_won = get_card_by_number(next_card_number)
+        [card_won, *get_cards_won_from_card(card_won)]
+      end
+    end
+  end
+
   class << self
     def part_one(input)
-      raise NotImplementedError
+      cards = input.map { |line| ScratchOffCard.from_string(line) }
+      cards.sum(&:score)
     end
 
     def part_two(input)
-      raise NotImplementedError
+      cards = input.map { |line| ScratchOffCard.from_string(line) }
+      game = ScratchOffGame.new(cards: cards)
+      game.all_cards.count
     end
   end
 end
--- spec/04_spec.rb
+++ spec/04_spec.rb
@@ -1,18 +1,95 @@
 require "./04"
 RSpec.describe Day04 do
+  describe Day04::ScratchOffCard do
+    context "properties" do
+      it "has a set of winning numbers and played_numbers" do
+        card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [1, 4, 5])
+        expect(card.winning_numbers).to eq [1, 2, 3]
+        expect(card.played_numbers).to eq [1, 4, 5]
+      end
+
+      it "has a card number" do
+        card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [1, 4, 5], card_number: 1)
+        expect(card.card_number).to eq 1
+      end
+
+      describe "#score" do
+        it "returns 1 if there is one match" do
+          card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [1, 4, 5])
+          expect(card.score).to eq 1
+        end
+
+        it "returns 2 if there are two matches" do
+          card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [1, 2, 5])
+          expect(card.score).to eq 2
+        end
+
+        it "returns 4 if there are three matches" do
+          card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [1, 2, 3])
+          expect(card.score).to eq 4
+        end
+
+        it "returns 8 if there are four matches" do
+          card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3, 4, 5], played_numbers: [1, 2, 3, 4, 9])
+          expect(card.score).to eq 8
+        end
+
+        it "returns 0 if there are no matches" do
+          card = Day04::ScratchOffCard.new(winning_numbers: [1, 2, 3], played_numbers: [4, 5, 6])
+          expect(card.score).to eq 0
+        end
+      end
+    end
+    context "initialization" do
+      it "can be created from a string" do
+        card = Day04::ScratchOffCard.from_string("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+        expect(card.card_number).to eq 1
+        expect(card.winning_numbers).to eq [41, 48, 83, 86, 17]
+        expect(card.played_numbers).to eq [83, 86, 6, 31, 17, 9, 48, 53]
+      end
+    end
+  end
+
+  describe Day04::ScratchOffGame do
+    context "properties" do
+      it "has a set of cards" do
+        game = Day04::ScratchOffGame.new(cards: [
+          Day04::ScratchOffCard.new(card_number: 1, winning_numbers: [1, 2, 3], played_numbers: [1, 4, 5]),
+          Day04::ScratchOffCard.new(card_number: 2, winning_numbers: [1, 2, 3], played_numbers: [4, 5, 6])
+        ])
+        expect(game.cards.size).to eq 2
+      end
+    end
+
+    describe "#all_cards" do
+      it "returns the the original cards with the copies that have been won" do
+        card_1 = Day04::ScratchOffCard.new(card_number: 1, winning_numbers: [1, 2, 3], played_numbers: [1, 4, 5])
+        card_2 = Day04::ScratchOffCard.new(card_number: 2, winning_numbers: [1, 2, 3], played_numbers: [4, 5, 6])
+        game = Day04::ScratchOffGame.new(cards: [
+          card_1,
+          card_2
+        ])
+
+        expect(game.all_cards).to match_array([
+          card_1,
+          card_2,
+          card_2
+        ])
+      end
+    end
+  end
+
   context "part 1" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/04.txt", chomp: true)
-      expect(Day04.part_one(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day04.part_one(input)).to eq 13
     end
   end
 
   context "part 2" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/04.txt", chomp: true)
-      expect(Day04.part_two(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day04.part_two(input)).to eq 30
     end
   end
 end
