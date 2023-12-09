59434eb42b066dc838f94b167a185315f243776b
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2023-12-09 16:20:05 -0500

    scaffold

---
+++ spec/07_spec.rb
@@ -0,0 +1,18 @@
+require "./07"
+RSpec.describe Day07 do
+  context "part 1" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/07.txt", chomp: true)
+      expect(Day07.part_one(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+
+  context "part 2" do
+    it "returns the correct answer for the example input" do
+      pending
+      input = File.readlines("spec/test_inputs/07.txt", chomp: true)
+      expect(Day07.part_two(input)).to eq 0 # TODO: replace with correct answer
+    end
+  end
+end
