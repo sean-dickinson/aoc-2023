9f95f5382ed8198b768e3e1a7380dac658a43ed2
sean-dickinson <90267290+sean-dickinson@users.noreply.github.com>
2023-12-09 14:57:41 -0500

    wip: part 1

--- 06.rb
+++ 06.rb
@@ -1,7 +1,66 @@
 module Day06
+  class RaceList
+    def initialize(input)
+      @races = parse_input(input)
+    end
+
+    def power
+      @races.map(&:ways_to_win).reduce(:*)
+    end
+
+    private
+
+    def parse_input(input)
+      times = parse_line(input.first)
+      distances = parse_line(input.last)
+      times.zip(distances).map { |time, distance| Race.new(time_allowed: time, record_distance: distance) }
+    end
+
+    # @param line [String]
+    def parse_line(line)
+      line.split(":").last.split.map(&:to_i)
+    end
+  end
+
+  class Race
+    def initialize(time_allowed:, record_distance:)
+      @time_allowed = time_allowed
+      @record_distance = record_distance
+    end
+
+    def ways_to_win
+      (0..@time_allowed).count do |charge_time|
+        boat.distance_for(charge_time: charge_time) > @record_distance
+      end
+    end
+
+    private
+
+    def boat
+      @boat ||= Boat.new(total_time: @time_allowed)
+    end
+  end
+
+  class Boat
+    def initialize(total_time:, speed_multiplier: 1)
+      @total_time = total_time
+      @speed_multiplier = speed_multiplier
+    end
+
+    def distance_for(charge_time:)
+      speed(charge_time) * (@total_time - charge_time)
+    end
+
+    private
+
+    def speed(charge_time)
+      @speed_multiplier * charge_time
+    end
+  end
+
   class << self
     def part_one(input)
-      raise NotImplementedError
+      RaceList.new(input).power
     end
 
     def part_two(input)
--- spec/06_spec.rb
+++ spec/06_spec.rb
@@ -1,10 +1,34 @@
 require "./06"
 RSpec.describe Day06 do
+  describe Day06::Boat do
+    describe "#distance_for" do
+      it "returns the distance for a given charge time" do
+        boat = Day06::Boat.new(total_time: 7)
+        expect(boat.distance_for(charge_time: 0)).to eq 0
+        expect(boat.distance_for(charge_time: 1)).to eq 6
+        expect(boat.distance_for(charge_time: 2)).to eq 10
+        expect(boat.distance_for(charge_time: 3)).to eq 12
+        expect(boat.distance_for(charge_time: 4)).to eq 12
+        expect(boat.distance_for(charge_time: 5)).to eq 10
+        expect(boat.distance_for(charge_time: 6)).to eq 6
+        expect(boat.distance_for(charge_time: 7)).to eq 0
+      end
+    end
+  end
+
+  describe Day06::Race do
+    describe "#ways_to_win" do
+      it "returns the number of ways to beat the record" do
+        race = Day06::Race.new(time_allowed: 7, record_distance: 9)
+        expect(race.ways_to_win).to eq 4
+      end
+    end
+  end
+
   context "part 1" do
     it "returns the correct answer for the example input" do
-      pending
       input = File.readlines("spec/test_inputs/06.txt", chomp: true)
-      expect(Day06.part_one(input)).to eq 0 # TODO: replace with correct answer
+      expect(Day06.part_one(input)).to eq 288
     end
   end
 
